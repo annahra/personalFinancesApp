7d1c2dcb80e3a768f8c0933017a4aec77d1efa5f
auna <annah.ramones@gmail.com>
2020-06-24 04:04:05 -0700

    [annah] used a guard on the intro page, so that user only sees it once, once they have selected their currency. if they haven't selected a currency, the menu page will never show, as only the intro page will

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { IntroGuard } from './guards/intro.guard';
 
 const routes: Routes = [
   {
-    path: '',
-    redirectTo: 'intro',
-    pathMatch: 'full'
+    path: 'menu',
+    loadChildren: () => import('./pages/menu/menu.module').then( m => m.MenuPageModule),
+    canActivate: [ IntroGuard ]
   },
   {
     path: 'intro',
     loadChildren: () => import('./pages/intro/intro.module').then( m => m.IntroPageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'menu',
+    pathMatch: 'full'
   },
 ];
 
